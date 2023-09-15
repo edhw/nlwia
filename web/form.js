4d4c0b792281d0e35f2122077ae9d0920fb5098d
edhw <edhw@live.com>
2023-09-15 17:15:10 -0300

    aula 3
    inicio até 14:20

--- web/form.js
+++ web/form.js
@@ -6,10 +6,13 @@ form.addEventListener("submit", (event) => {
   event.preventDefault()
 
   const videoURL = input.value
- 
-  if(!videoURL.includes("shorts")) {
-   return content.textContent = "Esse vídeo não é um Shorts. Verifique seu link."
+
+  if (!videoURL.includes("shorts")) {
+    return (content.textContent =
+      "Esse vídeo não é um Shorts. Verifique seu link.")
   }
 
-  videoURL.split()
+  const [_, params] = videoURL.split("/shorts/")
+  const [videoID] = params.split("?si")
+  console.log(params)
 })
